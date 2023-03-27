64ea93f5ae3ecc0e255aa9448f79a5097691bc6d
Nathan11Kruit <37533108+Nathan11Kruit@users.noreply.github.com>
2023-03-27 13:07:11 +0200

    add navbar remove logo

---
+++ src/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return(
+        <nav className="navbar">
+            <a className="logo-text">hallo</a>            
+            <a href="/">home</a>
+            <a href="/">profiel</a>
+            <a href="/">contact</a>        
+        </nav>
+
+    )
+
+}
+
+export default Navbar;
