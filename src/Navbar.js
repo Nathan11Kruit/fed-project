466da26196c75c5857bdae82f3cbdf3108b5ea08
Nathan11Kruit <37533108+Nathan11Kruit@users.noreply.github.com>
2023-03-30 13:25:08 +0200

    Begin navbar
    kleur/logo toegevoegd

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,13 +1,41 @@
 const Navbar = () => {
     return(
-        <nav className="navbar">
-            <a className="logo-text">hallo</a>            
-            <a href="/">home</a>
-            <a href="/">profiel</a>
-            <a href="/">contact</a>        
-        </nav>
-
+        <nav className="navbar navbar-expand-lg ">
+        <a className="navbar-brand" href="/">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/2560px-International_Pok%C3%A9mon_logo.svg.png" width="" height="30" alt="" ></img>
+        </a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarText">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <a className="nav-link" href="/">Home</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/">Profile</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/">Favorites</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/">Log out</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/">Favorites</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/">Login</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/">Nog iets</a>
+            </li>
+          </ul>
+          
+        </div>
+      </nav>
     )
+       
 
 }
 
