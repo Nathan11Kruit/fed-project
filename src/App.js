0efb85833edda784fe0a57f4c5ec9c9423fa7a38
Nathan11Kruit <37533108+Nathan11Kruit@users.noreply.github.com>
2023-03-30 15:35:01 +0200

    Adding style
    Added CSS for pokemon info cards

--- src/App.js
+++ src/App.js
@@ -16,8 +16,7 @@ const [pokemonChosen, setPokemonChosen] = useState(false);
 const [pokemon, setPokemon] = useState({
       name: pokemonName,
       species: "",
-      type: "",
-      speed: "",
+      type: "",      
       img: "",
       imgShiny: "",
       hp: "",
@@ -56,8 +55,8 @@ const searchPokemon = () => {
 };
 
   return (
-    <div className="App">
-     <div className="container">
+    <div className="container">
+     <div className="App">
     
       <Navbar />
       
@@ -67,8 +66,7 @@ const searchPokemon = () => {
         <div className="h2"></div>
 
         <div className="row pt">
-            <div className="col"> <div className="displaySection">
-         
+            <div className="col"> <div className="displaySection">        
          
           <div className="col"><input type="text" onChange={(event => {setPokemonName(event.target.value)})}></input></div>
           
@@ -79,29 +77,32 @@ const searchPokemon = () => {
             </div>
             </div>
 
-            <div className="container pt-5">
+            <div className="container pokemon-pokedex pb-5">
+              <div className="row pokemon-pokedex-titel pt-2 pb-2">
+                <div className="col"><h3 className="text-center">Gekozen Pokemon</h3></div>
+              </div>
               <div className="row">
-              <div className="col-3">
-                <ul className="pokemon-info">
-                  <li className="pokemon-name">{pokemon.name}</li>
-                  <li className="pokemon-image"><img height="200" alt="" src={pokemon.img}/></li>
-                  <li className="pokemon-image-shiny"><img height="200" alt="" src={pokemon.imgShiny}/></li>
-
-                  <li className="pokemon-species">Hitpoints: {pokemon.hp}</li>
+                <div className="col-3"></div>
+              <div className="col-3 mt-5 pokemon-info">
+                <ul className="pt-2">
+                  <li className="pokemon-info-list-title pokemon-species"><h3 className='text-center'>{pokemon.name}</h3></li>
+                  <li className="pokemon-info-list-item pokemon-image"><img className='mx-auto d-block' height="200" alt="" src={pokemon.img}/></li>
+                  <li className="pokemon-info-list-item pokemon-image-shiny"><img height="20" alt="" src={pokemon.imgShiny}/></li>
+                  <li className="pokemon-species text-center">Hitpoints: {pokemon.hp}</li>
 
 
 
               </ul>
               </div>
-              <div className="col-3">
-                <ul className="pokemon-stats">
-                  <li className="pokemon-species">Species: {pokemon.species}</li>
-                  <li className="pokemon-attack">Attack: {pokemon.attack}</li>
-                  <li className="pokemon-defense">Defense: {pokemon.defense}</li>
-                  <li className="pokemon-special-attack">Special Attack: {pokemon.specialAttack}</li>
-                  <li className="pokemon-special-defense">Special Defense: {pokemon.specialDefense}</li>
-                  <li className="pokemon-speed">Speed: {pokemon.speed}</li>
-                  <li className="pokemon-type">Type: {pokemon.type}</li>
+              <div className="col-3 mt-5 pokemon-stats">
+                <ul className=" pt-2 ">
+                  <li className="pokemon-stats-list-title pokemon-species"><h3 className='text-center'>Stats:</h3></li>
+                  <li className="pokemon-stats-list-item pokemon-attack">Attack: {pokemon.attack}</li>
+                  <li className="pokemon-stats-list-item pokemon-defense">Defense: {pokemon.defense}</li>
+                  <li className="pokemon-stats-list-item pokemon-special-attack">Special Attack: {pokemon.specialAttack}</li>
+                  <li className="pokemon-stats-list-item pokemon-special-defense">Special Defense: {pokemon.specialDefense}</li>
+                  <li className="pokemon-stats-list-item pokemon-speed">Speed: {pokemon.speed}</li>
+                  <li className="pokemon-stats-list-item pokemon-type">Type: {pokemon.type}</li>
 
 
 
@@ -113,8 +114,8 @@ const searchPokemon = () => {
        
           
         </div>
-      <Footer />
       </div>
+      <Footer />
 
     </div>
   );
