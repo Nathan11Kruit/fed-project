d803e253e26493358079116ae2de7862defedcbc
Nathan11Kruit <37533108+Nathan11Kruit@users.noreply.github.com>
2023-03-31 15:41:14 +0200

    Begin footer
    Added some style and socials

--- src/Footer.js
+++ src/Footer.js
@@ -1,7 +1,36 @@
 const Footer = () => {
     return(
-        <h2 className="title">Dit is footer element.</h2>
+        <>
+        <div className="container footer">
+        <div className="row">
+            <div className="col-2"></div>
+            <div className="col-2 mt-2 text-center">Socials:
+             <div className="row icons mt-4">
+                <div className="col-1">
+                  <img src="https://cdn-icons-png.flaticon.com/512/2111/2111463.png" alt="" className="instagram" width="25" />
+                  </div>
+                  <div className="col-1"></div>
+                <div className="col-1">
+                  <img src="https://cdn-icons-png.flaticon.com/512/4494/4494475.png" alt="" className="instagram" width="25" />
+                  </div>
+                  <div className="col-1"></div>
 
+                  <div className="col-1">
+                  <img src="https://cdn-icons-png.flaticon.com/512/4494/4494477.png" alt="" className="instagram" width="25" />
+                  </div>
+                  <div className="col-1"></div>
+
+                  <div className="col-1">
+                  <img src="https://cdn-icons-png.flaticon.com/512/733/733585.png" alt="" className="instagram" width="25" />
+                  </div>
+                  <div className="col-1"></div>
+
+                </div>
+             </div>
+            <div className="col-4 mt-2 text-center">Andere Pokemon plekken</div>
+
+        </div></div>
+        </>
     )
 
 }
