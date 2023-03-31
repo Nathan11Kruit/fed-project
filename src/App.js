503bd95b9089f7913d1df48e958249d71ab5a003
Nathan11Kruit <37533108+Nathan11Kruit@users.noreply.github.com>
2023-03-31 15:41:43 +0200

    Random generator
    Changed math function now finally works..

--- src/App.js
+++ src/App.js
@@ -1,20 +1,48 @@
 import './App.css';
 import "./navbar.css";
 import "./home.css";
+import "./footer.css";
+
 import Navbar from './Navbar';
 import Home from './Home';
 import Footer from './Footer';
 import { useState } from 'react'; 
 import Axios from 'axios';
 
-
-
 function App() {
-// create fields
+
+
+
+  
+  const [num, setNum] = useState(false);
+
+
+  function randomPokemon(min,max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)  };
+
+    function randomNewPokemon(){
+    const random = Math.floor(Math.random() * 256 + 1);
+    Axios.get(`https://pokeapi.co/api/v2/pokemon/${random}`).then((response)=>{
+      setPokemon({
+        name: response.data.name,
+        species: response.data.species.name,
+        type: response.data.types[0].type.name,
+        img: response.data.sprites.front_default,
+        imgShiny: response.data.sprites.front_shiny,
+        hp: response.data.stats[0].base_stat,
+        attack: response.data.stats[1].base_stat,
+        defense: response.data.stats[2].base_stat,
+        specialAttack: response.data.stats[3].base_stat,
+        specialDefense: response.data.stats[4].base_stat,
+        speed: response.data.stats[5].base_stat,
+      });
+    }
+    );    
+  }; 
+    // create fields
 const [pokemonName, setPokemonName] = useState("");
-const [pokemonChosen, setPokemonChosen] = useState(false);
 const [pokemon, setPokemon] = useState({
-      name: pokemonName,
+      name: num,
       species: "",
       type: "",      
       img: "",
@@ -25,15 +53,12 @@ const [pokemon, setPokemon] = useState({
       specialAttack: "",
       specialDefense: "",
       speed: "",
-
 });
-
-// get from api
-const searchPokemon = () => {
+// search pokemon in database by name
+const searchPokemon = () => { 
   Axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`).then((response)=>{
-    console.log(response);
     setPokemon({
-      name: pokemonName,
+      name: response.data.name,
       species: response.data.species.name,
       type: response.data.types[0].type.name,
       img: response.data.sprites.front_default,
@@ -44,39 +69,35 @@ const searchPokemon = () => {
       specialAttack: response.data.stats[3].base_stat,
       specialDefense: response.data.stats[4].base_stat,
       speed: response.data.stats[5].base_stat,
-
-
-     
-      
     });
-    setPokemonChosen(true);
   }
   );
 };
-
   return (
     <div className="container">
-     <div className="App">
-    
-      <Navbar />
-      
-               
+     <div className="App">    
+      <Navbar />       
       <div className="home"> 
-         <h2 className="home-title pt-5">Kies je Pokemon</h2>
-        <div className="h2"></div>
-
         <div className="row pt">
-            <div className="col"> <div className="displaySection">        
-         
-          <div className="col"><input type="text" onChange={(event => {setPokemonName(event.target.value)})}></input></div>
-          
-          <div className="col pt-3"><button onClick={searchPokemon}>search database</button></div>
-          </div></div>
-          </div>
-            <div className="col"><h2 className='home-title pt-5'>Of gebruik de random generator!</h2></div>
-            </div>
-            </div>
+            <div className="col-5"> 
+            <h2 className="home-title pt-5">Kies je favoriete Pokemon</h2>
 
+              <input type="text" onChange={(event => {setPokemonName(event.target.value);})}></input>     
+              
+              <button type="button" className="btn btn-danger" onClick={searchPokemon}>search database</button>
+              <h2 className='home-title pt-5'>Weet je nog niet wie je favoriet is? Gebruik dan de knop hieronder!</h2>
+              <button type="button" className="btn btn-warning"  onClick={event => {randomNewPokemon();   }}   >Willekeurige pokemon kiezen!</button>
+
+
+
+            </div>
+            <div className="col-6">
+                 
+              </div>
+            </div>
+        </div>
+            
+            </div>
             <div className="container pokemon-pokedex pb-5">
               <div className="row pokemon-pokedex-titel pt-2 pb-2">
                 <div className="col"><h3 className="text-center">Gekozen Pokemon</h3></div>
@@ -89,9 +110,6 @@ const searchPokemon = () => {
                   <li className="pokemon-info-list-item pokemon-image"><img className='mx-auto d-block' height="200" alt="" src={pokemon.img}/></li>
                   <li className="pokemon-info-list-item pokemon-image-shiny"><img height="20" alt="" src={pokemon.imgShiny}/></li>
                   <li className="pokemon-species text-center">Hitpoints: {pokemon.hp}</li>
-
-
-
               </ul>
               </div>
               <div className="col-3 mt-5 pokemon-stats">
@@ -103,20 +121,11 @@ const searchPokemon = () => {
                   <li className="pokemon-stats-list-item pokemon-special-defense">Special Defense: {pokemon.specialDefense}</li>
                   <li className="pokemon-stats-list-item pokemon-speed">Speed: {pokemon.speed}</li>
                   <li className="pokemon-stats-list-item pokemon-type">Type: {pokemon.type}</li>
-
-
-
-
-
                </ul>
-              </div>
-        
-       
-          
+              </div>    
         </div>
       </div>
       <Footer />
-
     </div>
   );
 }
