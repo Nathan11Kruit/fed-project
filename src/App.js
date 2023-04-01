6c6050c5e8a91d88e7a05a88821b2514e6fe0fc3
Nathan11Kruit <37533108+Nathan11Kruit@users.noreply.github.com>
2023-04-01 16:16:41 +0200

    Styling
    For landing image and cleaning up code

--- src/App.js
+++ src/App.js
@@ -94,53 +94,48 @@ const searchPokemon = () => {
      <div className="App">    
       <Navbar />       
       <div className="home"> 
-        <div className="row pt">
-            <div className="col-5"> 
+        <div className="row landing-image align-items-end">       
+          <div className="col-3"></div>
+            <div className="col-6 "> 
             <h2 className="home-title pt-5">Kies je favoriete Pokemon</h2>
-
-              <input type="text" onChange={(event => {setPokemonName(event.target.value);})}></input>     
-              
-              <button type="button" className="btn btn-danger" onClick={searchPokemon}>search database</button>
-              <h2 className='home-title pt-5'>Weet je nog niet wie je favoriet is? Gebruik dan de knop hieronder!</h2>
-              <button type="button" className="btn btn-warning"  onClick={event => {randomNewPokemon();   }}   >Willekeurige pokemon kiezen!</button>
-
-
-
-            </div>
-            <div className="col-6">
-                 
-              </div>
-            </div>
-        </div>
-            
-            </div>
+              <div className="row pt-3">
+               <div className="col-2"></div>
+                <div className="col-5"><input placeholder="Pokemon naam" className="form-control pokemon-name-input" type="text" onChange={(event => {setPokemonName(event.target.value);})}></input></div>              
+                <div className="col-3"><button type="button" className="btn btn-danger" onClick={searchPokemon}>Zoek Pokemon</button></div>
+              </div>              
+              <h2 className='home-title pt-5 pb-3'>Weet je nog niet wie je favoriet is? Gebruik dan de knop hieronder!</h2>
+              <button type="button" className="btn btn-warning mb-3"  onClick={event => {randomNewPokemon();   }}   >Willekeurige pokemon kiezen!</button>
+             </div>           
+          </div>
+        </div>            
+      </div>
             <div className="container pokemon-pokedex pb-5">
               <div className="row pokemon-pokedex-titel pt-2 pb-2">
                 <div className="col"><h3 className="text-center chosen-pokemon-title">Gekozen Pokemon</h3></div>
               </div>
               <div className="row">
                 <div className="col-3"></div>
-              <div className="col-3 mt-5 pokemon-info">
-                <ul className="pt-2">
-                  <li className="pokemon-info-list-title pokemon-species"><h3 className='text-center'>{pokemon.name}</h3></li>
-                  <li className="pokemon-info-list-item pokemon-image"><img id="pokemon-img" className='mx-auto d-block' height="200" alt="" src={pokemon.img}/><img height="20" alt="" src={pokemon.imgShiny}/></li>
-                  <li className="pokemon-info-list-item pokemon-image-shiny"><button className="switch" onClick={shinySwitch}></button></li>
-                  <li className="pokemon-species text-center">Hitpoints: {pokemon.hp}</li>
-              </ul>
-              </div>
-              <div className="col-3 mt-5 pokemon-stats">
-                <ul className=" pt-2 ">
-                  <li className="pokemon-stats-list-title pokemon-species"><h3 className='text-center'>Stats:</h3></li>
-                  <li className="pokemon-stats-list-item pokemon-attack">Attack: {pokemon.attack}</li>
-                  <li className="pokemon-stats-list-item pokemon-defense">Defense: {pokemon.defense}</li>
-                  <li className="pokemon-stats-list-item pokemon-special-attack">Special Attack: {pokemon.specialAttack}</li>
-                  <li className="pokemon-stats-list-item pokemon-special-defense">Special Defense: {pokemon.specialDefense}</li>
-                  <li className="pokemon-stats-list-item pokemon-speed">Speed: {pokemon.speed}</li>
-                  <li className="pokemon-stats-list-item pokemon-type">Type: {pokemon.type}</li>
-               </ul>
-              </div>    
-        </div>
-      </div>
+                <div className="col-3 mt-5 pokemon-info">
+                  <ul className="pt-2">
+                    <li className="pokemon-info-list-title pokemon-species"><h3 className='text-center'>{pokemon.name}</h3></li>
+                    <li className="pokemon-info-list-item pokemon-image"><img id="pokemon-img" className='mx-auto d-block' height="200" alt="" src={pokemon.img}/><img height="20" alt="" src={pokemon.imgShiny}/></li>
+                    <li className="pokemon-info-list-item pokemon-image-shiny"><button className="switch" onClick={shinySwitch}></button></li>
+                    <li className="pokemon-species text-center">Hitpoints: {pokemon.hp}</li>
+                  </ul>
+                </div>
+                <div className="col-3 mt-5 pokemon-stats">
+                  <ul className=" pt-2 ">
+                    <li className="pokemon-stats-list-title pokemon-species"><h3 className='text-center'>Stats:</h3></li>
+                    <li className="pokemon-stats-list-item pokemon-attack">Attack: {pokemon.attack}</li>
+                    <li className="pokemon-stats-list-item pokemon-defense">Defense: {pokemon.defense}</li>
+                    <li className="pokemon-stats-list-item pokemon-special-attack">Special Attack: {pokemon.specialAttack}</li>
+                    <li className="pokemon-stats-list-item pokemon-special-defense">Special Defense: {pokemon.specialDefense}</li>
+                    <li className="pokemon-stats-list-item pokemon-speed">Speed: {pokemon.speed}</li>
+                    <li className="pokemon-stats-list-item pokemon-type">Type: {pokemon.type}</li>
+                 </ul>
+                </div>    
+               </div>
+            </div>
       <Footer />
     </div>
   );
