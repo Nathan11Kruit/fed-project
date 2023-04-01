3b8f56ef5ca244ac441e442f7cc226ba5980888c
Nathan11Kruit <37533108+Nathan11Kruit@users.noreply.github.com>
2023-04-01 14:45:06 +0200

    Footer
    Added resources, added style

--- src/Footer.js
+++ src/Footer.js
@@ -2,9 +2,10 @@ const Footer = () => {
     return(
         <>
         <div className="container footer">
-        <div className="row">
+        <div className="row pt-3 pb-3">
             <div className="col-3"></div>
-            <div className="col-2 mt-2 text-center">Socials:
+            <div className="col-2 mt-2 footer-socials ">
+                <h6 className="text-center">Socials:</h6>
              <div className="row icons mt-4">
                 <div className="col-1">
                   <img src="https://cdn-icons-png.flaticon.com/512/2111/2111463.png" alt="" className="instagram" width="25" />
@@ -28,14 +29,29 @@ const Footer = () => {
                 </div>
 
              </div>
-             <div className="col-1"></div>
+             <div className="col-2 mt-2">
+                <h6 className="">Handige links</h6>
+                    <ul className="useful-links">
+                        <li className="useful-links-list-item"><a href="pokemon.com" className="link-light">Terms of Use</a></li>
+                        <li className="useful-links-list-item"><a href="pokemon.com" className="link-light">Privacy</a></li>
+                        <li className="useful-links-list-item"><a href="pokemon.com" className="link-light">Notice</a></li>
+                        <li className="useful-links-list-item"><a href="pokemon.com" className="link-light">Cookie Page</a></li>
+                        <li className="useful-links-list-item"><a href="pokemon.com" className="link-light">Legal Info</a></li>
+                        <li className="useful-links-list-item"><a href="pokemon.com" className="link-light">Security</a></li>
 
-            <div className="col-4 mt-2 "><h5 className="text-center">Andere Pokemon plekken</h5>
-            <ul className="pokemon-places text-end">
-                <li className="pokemon-places-list-item">Pokemon.com</li>
-                <li className="pokemon-places-list-item">Dikke turbo</li>
-                <li className="pokemon-places-list-item">Floepie</li>
-                <li className="pokemon-places-list-item">Doebie</li>
+                    </ul>
+
+               
+
+
+             </div>
+
+            <div className="col-2 mt-2 footer-places "><h6 className="text-center">Andere Pokemon plekken</h6>
+            <ul className="pokemon-places text-start">
+                <li className="pokemon-places-list-item"><a href="https://www.pokemon.com/us" className="link-light">Pokemon.com</a> </li>
+                <li className="pokemon-places-list-item"><a href="https://pokeapi.co/" className="link-light">Pokemon API</a></li>
+                <li className="pokemon-places-list-item"><a href="https://www.portal-pokemon.com/" className="link-light">Pokemon Startpagina</a></li>
+                <li className="pokemon-places-list-item"><a href="pokemon.com" className="link-light">Pokemon.com</a></li>
 
             </ul>
             
@@ -43,7 +59,12 @@ const Footer = () => {
             </div>
 
 
-        </div></div>
+        </div>
+        <div className="row">
+            <div className="col-4"></div>
+            <div className="col-4 text-center"><p className="copyright pt-2">Copyright 2023 - Nathan Kruit</p></div>
+            </div>
+            </div>
         </>
     )
 
