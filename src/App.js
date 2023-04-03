e6702de9a2590f904716a610480722a71a5f2586
Nathan11Kruit <37533108+Nathan11Kruit@users.noreply.github.com>
2023-04-03 18:30:30 +0200

    Creating bottom list
    Creating a list at the bottom that displays pokemon the user has designated to go here.

--- src/App.js
+++ src/App.js
@@ -8,37 +8,42 @@ import Home from './Home';
 import Footer from './Footer';
 import { useState } from 'react'; 
 import Axios from 'axios';
-import { Carousel, CarouselItem, CarouselCaption } from 'bootstrap';
 
 
 
 function App() { 
-  function randomPokemon(min,max) {
-    return Math.floor(Math.random() * (max - min + 1) + min)  };
-    const [pokemonRandom, setPokemonRandom] = useState("");
-    function randomNewPokemon(){
-    const random = Math.floor(Math.random() * 256 + 1)
-    setPokemonRandom(random);;
-    Axios.get(`https://pokeapi.co/api/v2/pokemon/${random}`).then((response)=>{
-      console.log(`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${random}` + `/pngpokemon.img`);
-      console.log({random})
+ 
+  const [pokemonList, setPokemonList] = useState([]);
 
-      setPokemon({
-        name: response.data.name,
-        species: response.data.species.name,
-        type: response.data.types[0].type.name,
-        img: response.data.sprites.front_default,
-        imgShiny: response.data.sprites.front_shiny,
-        hp: response.data.stats[0].base_stat,
-        attack: response.data.stats[1].base_stat,
-        defense: response.data.stats[2].base_stat,
-        specialAttack: response.data.stats[3].base_stat,
-        specialDefense: response.data.stats[4].base_stat,
-        speed: response.data.stats[5].base_stat,
+    const [pokemonRandom, setPokemonRandom] = useState("");
+
+    function addToTeam(){
+      setPokemonList([...pokemonList, pokemon]);
+      setPokemon(pokemon);
+    }
+
+    function randomNewPokemon() { 
+      const min = 1;
+      const max = 1008;
+      const random = Math.floor(Math.random() * (max - min + 1)) + min;
+      setPokemonRandom(random);
+      Axios.get(`https://pokeapi.co/api/v2/pokemon/${random}`).then((response) => {
+        setPokemon({
+          name: response.data.name,
+          species: response.data.species.name,
+          type: response.data.types[0].type.name,
+          img: response.data.sprites.front_default,
+          imgShiny: response.data.sprites.front_shiny,
+          hp: response.data.stats[0].base_stat,
+          attack: response.data.stats[1].base_stat,
+          defense: response.data.stats[2].base_stat,
+          specialAttack: response.data.stats[3].base_stat,
+          specialDefense: response.data.stats[4].base_stat,
+          speed: response.data.stats[5].base_stat,
+        });
       });
     }
-    );    
-  }; 
+
   function getNumberFromImageSource(imageSource) {
     const regex = /(\d+)/; // regex to match one or more digits
     const match = regex.exec(imageSource);
@@ -99,36 +104,26 @@ const searchPokemon = () => {
   }
   );
 };
+
+
   return (
     <div className="container">
      <div className="App">    
       <Navbar />       
       <div className="home"> 
-        <div className="row landing-image">   
-        <div id="carouselExampleSlidesOnly" className="carousel slide" data-ride="carousel">
-  <div className="carousel-inner">   
-  <div className="carousel-item active">
-      <img className="d-block w-100" src="https://static.onecms.io/wp-content/uploads/sites/6/2022/12/16/Pokemon-121622-1.jpg" alt="First slide" />
-    </div>
-    <div className="carousel-item">
-      <img className="d-block w-100" src="https://images3.alphacoders.com/975/975819.jpg" alt="Second slide" />
-    </div>
-    <div className="carousel-item">
-      <img className="d-block w-100" src="..." alt="Third slide" />
-    </div>
-            <div className="col-6 mt-6"> 
-              <div className="landing-image-text-field">
-              <h2 className="home-title pt-5">Kies je favoriete Pokemon</h2>
-                <div className="row pt-3">
-                <div className="col-2"></div>
-                  <div className="col-5"><input placeholder="Pokemon naam" className="form-control pokemon-name-input" type="text" onChange={(event => {setPokemonName(event.target.value);})}></input></div>              
-                  <div className="col-3"><button type="button" className="btn btn-danger" onClick={searchPokemon}>Zoek Pokemon</button></div>
-                </div>              
-                <h2 className='home-title pt-5 pb-3'>Weet je nog niet wie je favoriet is? Gebruik dan de knop hieronder!</h2>
-                <button type="button" className="btn btn-warning mb-3 random-pokemon-btn"  onClick={event => {randomNewPokemon();   }}   >Willekeurige pokemon kiezen!</button>
-              </div> 
-             </div>          
-          </div>
+        <div className="row landing-image">           
+          <div className="col-6 mt-6"> 
+            <div className="landing-image-text-field">
+            <h2 className="home-title pt-5">Kies je favoriete Pokemon</h2>
+              <div className="row pt-3">
+               <div className="col-2"></div>
+                <div className="col-5"><input placeholder="Pokemon naam" className="form-control pokemon-name-input" type="text" onChange={(event => {setPokemonName(event.target.value);})}></input></div>              
+                <div className="col-3"><button type="button" className="btn btn-danger" onClick={searchPokemon}>Zoek Pokemon</button></div>
+              </div>              
+              <h2 className='home-title pt-5 pb-3'>Weet je nog niet wie je favoriet is? Gebruik dan de knop hieronder!</h2>
+              <button type="button" className="btn btn-warning mb-3 random-pokemon-btn"  onClick={event => {randomNewPokemon();   }}   >Willekeurige pokemon kiezen!</button>
+             </div> 
+            </div>
           </div> 
         </div>            
       </div>
@@ -142,8 +137,9 @@ const searchPokemon = () => {
             <ul className="pt-2">
               <li className="pokemon-info-list-title pokemon-species"><h3 className='text-center'>{pokemon.name}</h3></li>
               <li className="pokemon-info-list-item pokemon-image"><img id="pokemon-img" className='mx-auto d-block' height="200" alt="" src={pokemon.img}/></li>
-              <li className="pokemon-info-list-item pokemon-image-shiny mb-4 mx-auto text-center img-fluid"><button className="switch btn btn-primary" onClick={shinySwitch}>Shiny</button><button className="switch btn btn-info" onClick={normalSwitch}>Normal</button></li>
               <li className="pokemon-species text-center">Hitpoints: {pokemon.hp}</li>
+              <li className="pokemon-info-list-item pokemon-image-shiny mt-3 mx-auto text-center img-fluid"><button className="switch btn btn-primary" onClick={shinySwitch}>Shiny</button><button className="switch btn btn-info" onClick={normalSwitch}>Normal</button></li>
+              <li className="pokemon-info-list-item pokemon-image text-center mt-2"><button className="switch btn btn-info btn-add-to-team" onClick={addToTeam}>Aan team toevoegen</button></li>
             </ul>
           </div>
           <div className="col-3 mt-5 pokemon-stats">
@@ -157,10 +153,24 @@ const searchPokemon = () => {
               <li className="pokemon-stats-list-item pokemon-type">Type: {pokemon.type}</li>
             </ul>
           </div>    
-          </div>
       </div>
-      <Footer />
     </div>
+    <div className="container">
+      <div className="row pokemon-pokedex-titel">
+      <h3 className="chosen-pokemons text-center chosen-pokemon-title">Dit is je team!</h3>
+      </div>
+      <div className="row">
+
+        {pokemonList.map(pokemon => (
+          
+          <div className="col-2"><h3 className='text-center chosen-pokemons-name'>{pokemon.name}</h3><img id="pokemon-img" className='mx-auto d-block' height="200" alt="" src={pokemon.img}/></div>
+        
+            
+        ))}
+         
+      </div>
+    </div>
+      <Footer />
     </div>
 
   );
---
+++ src/List.js
@@ -0,0 +1,89 @@
+import React, { useState } from 'react';
+
+function List() {
+  const [items, setItems] = useState([]);
+  const [newItem, setNewItem] = useState('');
+
+  function handleClick() {
+    setItems(prevItems => [...prevItems, newItem]);
+    setNewItem('');
+  }
+
+  return (
+    
+      <div className="col-1">
+  <ul className="pt-2">    
+
+        {items.map(item => (
+            <>
+            <li className="pokemon-info-list-title pokemon-species"><h3 className='text-center'>test</h3></li>
+            <li className="pokemon-info-list-item pokemon-image"><img id="pokemon-img" className='mx-auto d-block' height="200" alt="" src=""/></li>
+             </>
+
+        ))}
+      </ul>
+      <input type="text" value={newItem} onChange={e => setNewItem(e.target.value)} />
+      <button onClick={handleClick}>Add Item</button>
+    </div>
+  );
+}
+
+dit werkte best goed::
+
+const [results, setResults] = useState([]);
+const handleClick = () => {
+  const newResult = randomNewPokemon();
+  setResults(prevResults => [...prevResults, newResult]);
+};
+
+{results.map((result, index) => (
+    <li key={index}>{result}</li>
+  ))}
+// old functions
+function randomNewPokemon() { 
+    const min = 1;
+    const max = 1008;
+    const random = Math.floor(Math.random() * (max - min + 1)) + min;
+    setPokemonRandom(random);
+    Axios.get(`https://pokeapi.co/api/v2/pokemon/${random}`).then((response) => {
+      setPokemon({
+        name: response.data.name,
+        species: response.data.species.name,
+        type: response.data.types[0].type.name,
+        img: response.data.sprites.front_default,
+        imgShiny: response.data.sprites.front_shiny,
+        hp: response.data.stats[0].base_stat,
+        attack: response.data.stats[1].base_stat,
+        defense: response.data.stats[2].base_stat,
+        specialAttack: response.data.stats[3].base_stat,
+        specialDefense: response.data.stats[4].base_stat,
+        speed: response.data.stats[5].base_stat,
+      });
+      setPokemonList([...pokemonList, pokemon]);
+    });
+  }
+
+
+  function randomNewPokemon(){
+    
+  const random = Math.floor(Math.random() * 256 + 1)
+  setPokemonRandom(random);
+  Axios.get(`https://pokeapi.co/api/v2/pokemon/${random}`).then((response)=>{
+    setPokemon({
+      name: response.data.name,
+      species: response.data.species.name,
+      type: response.data.types[0].type.name,
+      img: response.data.sprites.front_default,
+      imgShiny: response.data.sprites.front_shiny,
+      hp: response.data.stats[0].base_stat,
+      attack: response.data.stats[1].base_stat,
+      defense: response.data.stats[2].base_stat,
+      specialAttack: response.data.stats[3].base_stat,
+      specialDefense: response.data.stats[4].base_stat,
+      speed: response.data.stats[5].base_stat,
+    });
+    setPokemonList([...pokemonList, pokemon]);
+  }
+  );    
+}; 
+export default List;
