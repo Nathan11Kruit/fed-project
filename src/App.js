cd7fae6b3f5ff20b0b84ec3552802c8008e6fbd4
Nathan11Kruit <37533108+Nathan11Kruit@users.noreply.github.com>
2023-04-03 14:21:14 +0200

    rewrote buttons
    Shiny switch now working by getting pokemon from image source number

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ import Home from './Home';
 import Footer from './Footer';
 import { useState } from 'react'; 
 import Axios from 'axios';
+import { Carousel, CarouselItem, CarouselCaption } from 'bootstrap';
+
+
 
 function App() { 
   function randomPokemon(min,max) {
@@ -36,23 +39,31 @@ function App() {
     }
     );    
   }; 
+  function getNumberFromImageSource(imageSource) {
+    const regex = /(\d+)/; // regex to match one or more digits
+    const match = regex.exec(imageSource);
+    return match ? Number(match[0]) : null; // convert the matched string to a number
+  }
   function shinySwitch(){
     let displayImage = document.getElementById('pokemon-img')
-    if(displayImage.src.match(`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon${pokemonRandom}` + `.png`)){
-      displayImage.src = (`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonRandom}` + `.png`)
+    const imageSource = displayImage.src;
+    const numberFromImageSource = getNumberFromImageSource(imageSource);
+    if(displayImage.src.match(`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${numberFromImageSource}` + `.png`)){
+      displayImage.src = (`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/${numberFromImageSource}` + `.png`)
     } else {
-      displayImage.src = (`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/${pokemonRandom}` + `.png`)
-    }
+      displayImage.src = (`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/${numberFromImageSource}` + `.png`)
+    }    
   }
   function normalSwitch(){
     let displayImage = document.getElementById('pokemon-img')
-    if(displayImage.src.match(`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/${pokemonRandom}` + `.png`)){
-      displayImage.src = (`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonRandom}` + `.png`)
+    const imageSource = displayImage.src;
+    const numberFromImageSource = getNumberFromImageSource(imageSource);
+    if(displayImage.src.match(`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/${numberFromImageSource}` + `.png`)){
+      displayImage.src = (`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${numberFromImageSource}` + `.png`)
     } else {
-      displayImage.src = (`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonRandom}` + `.png`)
+      displayImage.src = (`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${numberFromImageSource}` + `.png`)
     }
   }
-
     // create fields
 const [pokemonName, setPokemonName] = useState("");
 const [pokemon, setPokemon] = useState({
@@ -71,6 +82,7 @@ const [pokemon, setPokemon] = useState({
 // search pokemon in database by name
 const searchPokemon = () => { 
   Axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`).then((response)=>{
+    console.log(pokemonName)
     setPokemon({
       name: response.data.name,
       species: response.data.species.name,
@@ -92,19 +104,32 @@ const searchPokemon = () => {
      <div className="App">    
       <Navbar />       
       <div className="home"> 
-        <div className="row landing-image align-items-end">       
-          <div className="col-3"></div>
-            <div className="col-6 "> 
-            <h2 className="home-title pt-5">Kies je favoriete Pokemon</h2>
-              <div className="row pt-3">
-               <div className="col-2"></div>
-                <div className="col-5"><input placeholder="Pokemon naam" className="form-control pokemon-name-input" type="text" onChange={(event => {setPokemonName(event.target.value);})}></input></div>              
-                <div className="col-3"><button type="button" className="btn btn-danger" onClick={searchPokemon}>Zoek Pokemon</button></div>
-              </div>              
-              <h2 className='home-title pt-5 pb-3'>Weet je nog niet wie je favoriet is? Gebruik dan de knop hieronder!</h2>
-              <button type="button" className="btn btn-warning mb-3"  onClick={event => {randomNewPokemon();   }}   >Willekeurige pokemon kiezen!</button>
-             </div>           
+        <div className="row landing-image">   
+        <div id="carouselExampleSlidesOnly" className="carousel slide" data-ride="carousel">
+  <div className="carousel-inner">   
+  <div className="carousel-item active">
+      <img className="d-block w-100" src="https://static.onecms.io/wp-content/uploads/sites/6/2022/12/16/Pokemon-121622-1.jpg" alt="First slide" />
+    </div>
+    <div className="carousel-item">
+      <img className="d-block w-100" src="https://images3.alphacoders.com/975/975819.jpg" alt="Second slide" />
+    </div>
+    <div className="carousel-item">
+      <img className="d-block w-100" src="..." alt="Third slide" />
+    </div>
+            <div className="col-6 mt-6"> 
+              <div className="landing-image-text-field">
+              <h2 className="home-title pt-5">Kies je favoriete Pokemon</h2>
+                <div className="row pt-3">
+                <div className="col-2"></div>
+                  <div className="col-5"><input placeholder="Pokemon naam" className="form-control pokemon-name-input" type="text" onChange={(event => {setPokemonName(event.target.value);})}></input></div>              
+                  <div className="col-3"><button type="button" className="btn btn-danger" onClick={searchPokemon}>Zoek Pokemon</button></div>
+                </div>              
+                <h2 className='home-title pt-5 pb-3'>Weet je nog niet wie je favoriet is? Gebruik dan de knop hieronder!</h2>
+                <button type="button" className="btn btn-warning mb-3 random-pokemon-btn"  onClick={event => {randomNewPokemon();   }}   >Willekeurige pokemon kiezen!</button>
+              </div> 
+             </div>          
           </div>
+          </div> 
         </div>            
       </div>
       <div className="container pokemon-pokedex pb-5">
@@ -136,6 +161,8 @@ const searchPokemon = () => {
       </div>
       <Footer />
     </div>
+    </div>
+
   );
 }
 
